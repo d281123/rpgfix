640ae610e17f0bc255a6010ec382656a6b8975b6
d281123 <98954476+d281123@users.noreply.github.com>
2022-02-11 09:43:18 +0700

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 m.reply(wait)
-heum = await fetch(`https://hardianto.xyz/api/anime/random?nsfw=hentai&apikey=hardianto`)
+   heum = await fetch(`https://api-alphabot.herokuapp.com/api/nsfw/hentai?apikey=Alphabot`)
    if (!heum) throw json
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendButtonImg(m.chat, json, 'Wangy wangy wangy', '© R-Txzy', 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['hentai']
 handler.tags = ['hentai']
