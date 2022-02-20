38259cf5f2ca76e5bd6d71fe74e1e1111aa870d8
d281123 <98954476+d281123@users.noreply.github.com>
2022-02-20 10:22:44 +0700

    Add files via upload

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,11 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-m.reply(wait)
-   heum = await fetch(`https://api-alphabot.herokuapp.com/api/nsfw/hentai?apikey=Alphabot`)
+	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
+heum = await fetch(`https://hardianto.xyz/api/anime/random?nsfw=hentai&apikey=hardianto`)
    if (!heum) throw json
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, 'Wangy wangy wangy', '© RUSHBOTZ', 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendButtonImg(m.chat, json, 'Wangy wangy wangy', '©RUSHBOTZ', 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['hentai']
 handler.tags = ['hentai']
