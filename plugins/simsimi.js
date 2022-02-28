87a517b114e2002389e305a1f667ff72ad0633dd
d281123 <98954476+d281123@users.noreply.github.com>
2022-02-28 19:23:53 +0700

    Add files via upload

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
-  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/skatch?apikey=APIKEY&url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmrYfgiRt1lLwNgRMgMbID62qH3EQ2T1c6RUJEQMcoCWKqVVxAg7R_LfM&s=10`)
+  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/skatch?apikey=https://youtu.be/JNuiRPRysuk&url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmrYfgiRt1lLwNgRMgMbID62qH3EQ2T1c6RUJEQMcoCWKqVVxAg7R_LfM&s=10`)
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.jawaban == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
